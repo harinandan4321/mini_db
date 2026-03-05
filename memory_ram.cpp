f8867a4318d27629c72f5c266ea6231ba5b9673f
harinandanreddy <harinandanpalleti@gmail.com>
2026-03-05 12:26:19 +0530

    disk memory

--- memory_ram.cpp
+++ memory_ram.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
-#include <sstream>
-//string stream takes an input line and splits into parts based on whitespace
+#include <sstream> //string stream takes an input line and splits into parts based on whitespace
+#include <fstream> //contains both input aqnd output files
+
 using namespace std;
 
 class MiniDB {
     private:
     unordered_map<string,string> store;
+    void load () {
+    ifstream infile("data.txt");
+    if(!infile) return;
+    string key,value;
+    while(infile >> key >> value){store[key]=value;}
+    infile.close();
+    cout << "system databse is loaded. " << store.size() << "keys loaded." << endl;
+}
 
     public:
+    MiniDB () {
+        load();
+    }
     void set(string key,string value){
         store[key]=value;
-        cout << "saved [" << key << "->" << value << "]" << endl;
+        ofstream outfile("data.txt", ios::app);
+        if(outfile.is_open()) {
+            outfile << key << " " << value << endl;
+            outfile.close();
+            cout << "saved [" << key << "->" << value << "]" << endl;
+        }
+        else {
+            cout << "Error Access denied!" << endl;
+        }
+        
     }
     string get(string key){
         if(store.count(key)) {
@@ -21,6 +42,8 @@ class MiniDB {
         else {return "Error: key not found!";}
     }
 };
+
+
 
 int main() {
     // adding a cli so it can be interactive but doesnt have a permanent meory yet
